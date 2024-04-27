7083fc942aad736740894e61e3565cbe4476ea46
Joel Burget <joelburget@gmail.com>
2024-04-27 15:08:23 -0700

    Attempt to build a dashboard.

--- eval_polar.py
+++ eval_polar.py
@@ -1,7 +1,7 @@
 import torch
 
-# from sae_lens.training.evals import run_evals
-from sae_lens.training.session_loader import LMSparseAutoencoderSessionloader
+# from sae_lens.training.session_loader import LMSparseAutoencoderSessionloader
+from sae_lens.analysis.dashboard_runner import DashboardRunner
 
 if __name__ == "__main__":
     if torch.cuda.is_available():
@@ -11,13 +11,14 @@ if __name__ == "__main__":
     else:
         device = "cpu"
 
-    # layer = 8 # pick a layer you want.
-    # REPO_ID = "jbloom/GPT2-Small-SAEs"
-    # FILENAME = f"final_sparse_autoencoder_gpt2-small_blocks.{layer}.hook_resid_pre_24576.pt"
-    # path = hf_hub_download(repo_id=REPO_ID, filename=FILENAME)
-    model, sparse_autoencoder, activation_store = (
-        LMSparseAutoencoderSessionloader.load_pretrained_sae(
-            path="checkpoints/klnkkga3/final/gelu-2l_blocks.0.hook_mlp_out_8192_/"
-        )
+    sae_path = "checkpoints/klnkkga3/final/gelu-2l_blocks.0.hook_mlp_out_8192_/"
+    # model, sparse_autoencoder, activation_store = (
+    #     LMSparseAutoencoderSessionloader.load_pretrained_sae(path=sae_path)
+    # )
+
+    runner = DashboardRunner(
+        sae_path=sae_path,
+        wandb_artifact_path="joelb/polar/dashboard",
+        use_wandb=True,
     )
-    sparse_autoencoder.eval()
+    runner.run()
